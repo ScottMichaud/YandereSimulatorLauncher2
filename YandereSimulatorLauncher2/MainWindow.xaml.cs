45983924fec7ddf7c017c975f0a99f1c81237425
Scott Michaud <scott@scott-michaud.com>
2019-12-28 01:32:51 -0500

    WIP: Adding video to launcher.

--- YandereSimulatorLauncher2/MainWindow.xaml.cs
+++ YandereSimulatorLauncher2/MainWindow.xaml.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.ComponentModel;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO;
 
 namespace YandereSimulatorLauncher2
 {
@@ -22,8 +24,19 @@ namespace YandereSimulatorLauncher2
     {
         public MainWindow()
         {
+            UnpackVideoResources();
             InitializeComponent();
             HandleVisualStyles();
+        }
+
+        protected override sealed void OnClosing(CancelEventArgs e)
+        {
+            base.OnClosing(e);
+
+            if (e.Cancel == false)
+            {
+                DeleteVideoResources();
+            }
         }
 
         private void HandleVisualStyles()
@@ -46,7 +59,7 @@ namespace YandereSimulatorLauncher2
             Height = 701;
             AllowsTransparency = true;
             Background = Brushes.Transparent;
-            ShadowBorder.Margin = new Thickness(10);
+            ShadowBorder.Margin = new Thickness(13);
 
             //
         }
@@ -63,12 +76,41 @@ namespace YandereSimulatorLauncher2
             //
         }
 
+        private void UnpackVideoResources()
+        {
+            UnpackVideoFile(YandereSimulatorLauncher2.Properties.Resources.mainpanel_dere, "mainpanel-dere.wmv");
+            UnpackVideoFile(YandereSimulatorLauncher2.Properties.Resources.mainpanel_yan, "mainpanel-yan.wmv");
+        }
+
+        private void DeleteVideoResources()
+        {
+            if (File.Exists("mainpanel-dere.wmv"))
+            {
+                File.Delete("mainpanel-dere.wmv");
+            }
+
+            if (File.Exists("mainpanel-yan.wmv"))
+            {
+                File.Delete("mainpanel-yan.wmv");
+            }
+        }
+
+        private static void UnpackVideoFile(byte[] inResource, string inFilename)
+        {
+            File.WriteAllBytes(inFilename, inResource);
+        }
+
         private void TitleBar_MouseDown(object sender, MouseButtonEventArgs evt)
         {
             if (evt.ChangedButton == MouseButton.Left && evt.Handled == false)
             {
                 DragMove();
             }
+        }
+
+        private void MainPanelDereVideo_OnMediaEnded(object sender, RoutedEventArgs e)
+        {
+            MainPanelDereVideo.Position = new TimeSpan(0, 0, 1);
         }
     }
 }
