018d35daba3f41434bb6a084dcc41962b0a7460b
Scott Michaud <scott@scott-michaud.com>
2019-12-30 16:35:25 -0500

    Added branded logo icon

--- YandereSimulatorLauncher2/Properties/Resources.Designer.cs
+++ YandereSimulatorLauncher2/Properties/Resources.Designer.cs
@@ -61,6 +61,16 @@ namespace YandereSimulatorLauncher2.Properties {
         }
         
         /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Icon similar to (Icon).
+        /// </summary>
+        internal static System.Drawing.Icon icon_logo {
+            get {
+                object obj = ResourceManager.GetObject("icon-logo", resourceCulture);
+                return ((System.Drawing.Icon)(obj));
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized resource of type System.Byte[].
         /// </summary>
         internal static byte[] mainpanel_dere {
